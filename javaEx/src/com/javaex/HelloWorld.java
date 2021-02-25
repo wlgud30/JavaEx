bfabc8be9d5ee9bf19e7f40165b698c90b270110
강지형 <wlgud30@naver.com>
2021-02-25 14:46:59 +0900

    KJH Commit

---
+++ javaEx/src/com/javaex/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.javaex;
+
+// 주석 : 컴파일되지않는 설명문
+/*
+ * 여러줄 주석
+ * 
+ * */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
