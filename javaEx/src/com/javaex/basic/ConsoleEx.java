3a28ad21f1f3bfaf9f2d74c57b09d5c3c6bc5e04
강지형 <wlgud30@naver.com>
2021-02-26 15:49:15 +0900

    콘솔출력

---
+++ javaEx/src/com/javaex/basic/ConsoleEx.java
@@ -0,0 +1,29 @@
+package com.javaex.basic;
+
+public class ConsoleEx {
+	public static void main(String[] args) {
+		consoleOutputEx();
+	}
+	
+	//콘솔 출력
+	private static void consoleOutputEx() {
+		//표준 출력(stdout) : System.out
+		//표준 에러(stderr) : System.err
+		//메서드 print() -> 출력 후 개행을 하지 않음
+		//메서드 println() -> 출력  후 개행
+		//printf() 포맷출력 -> String 항목에서 정리
+		
+		System.out.print("Hello ");
+		System.out.println("Java");
+		System.out.println("NewLine");
+		
+		// 이스케이프 문자
+		// \n개행 \t탭 \"큰따옴표 \\역슬래시
+		System.out.println("Hello \nJava");	//강제 개행
+		System.out.println("Hello \t\tJava");	//탭
+		System.out.println("Hello \"Java\"");
+		
+		String filePath ="D:\\myFolder\\myfile.txt";
+		System.out.println(filePath);
+	}
+}
