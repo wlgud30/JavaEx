b90ce673bb83d777ce7c8d4a996e93646a522201
강지형 <wlgud30@naver.com>
2021-03-03 09:33:41 +0900

    문제

--- javaEx/src/com/javaex/practice01/Problem05.java
+++ javaEx/src/com/javaex/practice01/Problem05.java
@@ -11,16 +11,12 @@ public class Problem05 {
 	private static void problem05() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요");
-		int num;
-		int exNum = 0;
 		int result = 0;
 		for (int i = 0; i < 5; i++) {
 			System.out.print("숫자 :"); // 1
-			num = sc.nextInt();
-			if (num >= exNum) {
+			int num = sc.nextInt();
+			if(i==1||num>=result) {
 				result = num;
-			} else {
-				result = exNum;
 			}
 		}
 		System.out.println("최대값은 "+ result +" 입니다");
